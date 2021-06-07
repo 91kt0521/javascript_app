073cb90526f05918aad462d72fe4b183509b15f1
K_T <armn7guc.supbs.3@gmail.com>
2021-06-07 20:21:38 +0900

    [add]:連番で表示する

--- bingo/app.js
+++ bingo/app.js
@@ -9,23 +9,26 @@ var panelNum = 10;
 // 連番の配列を作成
 let Bingo = Array.from(new Array(75)).map((v, i)=> i + 1);
 
+// 1〜75まで連番で表示する
 var renderBingo = function (){
-    // 連番の配列から要素を取り出し変数にまとめる
+    var fragment = document.createDocumentFragment();
+    var Wrap;
+    // 連番の配列から要素を取り出し変数にまとめる(elemだと上手くいかない謎)
     Bingo.forEach(function(elem, index){
-        // ID属性を取得
-        var result_element = document.getElementById('result');
-
+        if (index % 15 === 0) {
+            Wrap = fragment.appendChild(document.createElement('div'));
+            Wrap.className = 'WrapDiv';
+        }
         //新しい要素を作成
-        var result_num_element = document.createElement('div');
+        var result_num_element = Wrap.appendChild(document.createElement('div'));
         result_num_element.className = 'bingo';
         result_num_element.textContent = elem;
-
-        // 指定した要素の末尾に新要素を挿入する
-        result_element.appendChild(result_num_element);
-
-        // console.log(elem);
-        // console.log(index+=1);
     });
+
+    // ID属性を取得
+    var result = document.getElementById('result');
+    // 指定した要素の末尾に新要素を挿入する
+    result.appendChild(fragment);
 }
 // 呼び出し
 renderBingo();
