0e90ae293d820ab21ca1d9fc16d76a115dfd8319
K_T <armn7guc.supbs.3@gmail.com>
2021-06-08 22:24:49 +0900

    [add]:大体の動作実装完了

--- bingo/app.js
+++ bingo/app.js
@@ -1,13 +1,14 @@
 var panelNum = document.getElementById('panelNum').innerHTML;
-// console.log(panelNum);
 var panelNum = 10;
-// console.log(panelNum);
 
-
-//とりあえず、view側に20個くらいベタが書きで番号を表示してそこからループに置き換えるようにしよう
 
 // 連番の配列を作成
 let Bingo = Array.from(new Array(75)).map((v, i)=> i + 1);
+
+var timers = [];
+var stopCount = 0;
+var panel = document.getElementById('panelNum');
+var btn0 = document.getElementById('btn0');
 
 // 1〜75まで連番で表示する
 var renderBingo = function (){
@@ -34,3 +35,61 @@ var renderBingo = function (){
 renderBingo();
 
 
+// SPINボタン押下後要素追加変更
+spinButton.addEventListener('click', function() {
+    // ボタン無効化
+    this.className = 'inactive';
+    btn0.className = 'btn';
+    panel.className = 'panel';
+    runSlot(0, panel);
+})
+
+/**
+ * SPINボタン押下後の処理
+ *
+ */
+function runSlot(n, panel){
+    // BINGOの数の乱数から値を取得
+    var num = Bingo[Math.floor(Math.random() * Bingo.length)];
+    bingoNum = num;
+    panel.innerHTML = num;
+
+    // タイマーをセット(関数を繰り返す)
+    timers[n] = setTimeout(function() {
+        runSlot(n, panel)
+    }, 25);
+}
+
+btn0.addEventListener('click', function() {
+    stopSlot(0, this);
+});
+
+
+/**
+ * ストップボタン押下後の処理
+ *
+ */
+function stopSlot (n, btn) {
+    btn.className = 'btn inactive';
+    // タイマー解除
+    clearTimeout(timers[n]);
+    // ランダムで選択されたbingoNumをBingoの配列から選定する
+    delArrayNum = Bingo.indexOf(bingoNum);
+    if (delArrayNum >= 0) {
+        // 配列から選定された数字をBingo配列から削除
+        Bingo.splice(delArrayNum, 1);
+    }
+
+    stopCount ++ ;
+    if(stopCount == 1) {
+        stopCount = 0;
+        // ボタン有効化
+        spinButton.className = '';
+
+        // bingoクラスのセレクタに一致するHTML要素を取得
+        var bingoDiv = document.querySelectorAll('.bingo');
+
+        // bingoDiv[bingoNum-1].innerHTML;
+        bingoDiv[bingoNum-1].className = 'bingo unmatched';
+    }
+}
